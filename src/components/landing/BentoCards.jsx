ab8f6cb49768c0ab762985bae11253b12baa15f9
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-08 01:54:57 +0530

    change title name and modification in bento grid in feature section

--- src/components/landing/BentoCards.jsx
+++ src/components/landing/BentoCards.jsx
@@ -3,17 +3,19 @@ function BentoCards({
   rowspan,
   colstart,
   rowstart,
+  colend,
+  rowend,
   imgsrc,
   title,
   para,
 }) {
   return (
     <div
-      className={`col-span-${colspan} row-span-${rowspan} col-start-${colstart} row-start-${rowstart} hover:scale-105 transition-all ease border-[0.5px] border-zinc-300 p-6 shadow-(--shadow-md) rounded-md overflow-hidden`}
+      className={`col-span-${colspan} row-span-${rowspan} col-start-${colstart} row-start-${rowstart} col-end-${colend} row-end-${rowend} hover:scale-105 transition-all ease border-[0.5px] border-zinc-300 p-6 shadow-(--shadow-md) rounded-md overflow-hidden`}
     >
       <h4>{title}</h4>
       <p className="text-zinc-500">{para}</p>
-      <img className="bg-center rounded-md" src={imgsrc} alt={title} />
+      <img className="bg-center rounded-md h-full" src={imgsrc} alt={title} />
     </div>
   );
 }
--- src/components/landing/Feature.jsx
+++ src/components/landing/Feature.jsx
@@ -9,15 +9,16 @@ function Feature() {
       <p className="xl:col-start-5 xl:col-end-9 md:col-start-4 md:col-end-6 col-start-3 col-end-6 text-zinc-500 text-center">
         DevFlow keeps every task visible, organized, and moving.
       </p>
-
       <div className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7 p-4">
-        <div className="grid grid-cols-4 grid-rows-8 gap-8 h-150">
+        <div className="grid grid-cols-4 grid-rows-8 gap-4 h-150">
           <BentoCards
             colspan={"2"}
             rowspan={"5"}
             colstart={"1"}
             rowstart={"1"}
-            imgsrc={"public/images/f1.png"}
+            colend={"3"}
+            rowend={"6"}
+            imgsrc={"/images/f1.png"}
             title={"Visual Workflow at a Glance"}
             para={"See every task clearly as it moves from idea to completion."}
           />
@@ -26,7 +27,9 @@ function Feature() {
             rowspan={"3"}
             colstart={"1"}
             rowstart={"6"}
-            imgsrc={"public/images/f2.png"}
+            colend={"3"}
+            rowend={"9"}
+            imgsrc={"/images/f2.png"}
             title={"Workflows That Make Sense"}
             para={
               "Built-in rules prevent invalid transitions and keep work flowing correctly."
@@ -37,7 +40,9 @@ function Feature() {
             rowspan={"3"}
             colstart={"3"}
             rowstart={"1"}
-            imgsrc={"public/images/f3.png"}
+            colend={"5"}
+            rowend={"4"}
+            imgsrc={"/images/f3.png"}
             title={"Find What Matters Instantly"}
             para={
               "Filter and search issues by status, priority, or title in seconds."
@@ -48,7 +53,9 @@ function Feature() {
             rowspan={"5"}
             colstart={"3"}
             rowstart={"4"}
-            imgsrc={"public/images/f4.png"}
+            colend={"5"}
+            rowend={"9"}
+            imgsrc={"/images/f4.png"}
             title={"Manage Tasks With Ease"}
             para={"Create, edit, and organize issues directly on the board."}
           />
