0bb3deda790bf4dee34259eb0416a8bf0e954e68
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-09 22:48:16 +0530

    adding step section in howitworks section

--- src/components/landing/HowItWorks.jsx
+++ src/components/landing/HowItWorks.jsx
@@ -17,7 +17,40 @@ function HowItWorks() {
         </div>
       </div>
       {/* HowitWorks */}
-      <div className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7"></div>
+      <div className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7">
+        <div className="flex xl:flex-row md:flex-row flex-col justify justify-between gap-2 h-120">
+          <div className="xl:flex-col md:flex-col flex justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
+            <div>
+              <h4>Create Tasks</h4>
+              <p className="text-zinc-500">
+                Create tasks for features, bugs, or improvements and organize
+                them instantly on the board.
+              </p>
+            </div>
+            <h4 className="xl:block md:block hidden">01</h4>
+          </div>
+          <div className="flex flex-col justify-between xl:border-l-[0.2px] xl:border-r-[0.2px] md:border-l-[0.2px] md:border-r-[0.2px] border-l-[0.2px] border-zinc-200 p-4">
+            <div>
+              <h4>Move Work</h4>
+              <p className="text-zinc-500">
+                Drag tasks across workflow stages. DevFlow enforces valid
+                transitions so work always moves in the right direction.
+              </p>
+            </div>
+            <h4 className="xl:block md:block hidden">02</h4>
+          </div>
+          <div className="flex flex-col justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
+            <div>
+              <h4>Stay Focused</h4>
+              <p className="text-zinc-500">
+                Use search and filters to instantly find tasks by status,
+                priority, or title.
+              </p>
+            </div>
+            <h4 className="xl:block md:block hidden">03</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
