6a513c9605309186f3b0d0df854c08d0a51e66aa
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-12 23:56:15 +0530

    fix responsive of howitwork green container

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,6 @@
 import Feature from "../components/landing/Feature";
 import HeroSection from "../components/landing/HeroSection";
 import HowItWorks from "../components/landing/HowItWorks";
-import MyComponent from "../components/landing/MyComponent";
 import Navbar from "../components/landing/Navbar";
 
 function LandingPage() {
