14eb0040291065bf38d42f9b2ffcaa489ac74415
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-16 23:46:46 +0530

    text add in footer

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full grid grid-cols-7 md:grid-cols-8 xl:grid-cols-12 gap-2 pt-8">
+      <div className="xl:col-start-3 xl:col-end-11 text-[200px] text-center bg-linear-to-b from-black to-white text-transparent bg-clip-text">
+        DEVFLOW
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
