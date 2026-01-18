d267b9d87d2e207782715622dc20eb08466462f3
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-18 22:21:43 +0530

    complete it using flex box ctasection

--- src/components/landing/CtaSection.jsx
+++ src/components/landing/CtaSection.jsx
@@ -2,15 +2,13 @@ import Ctabutton from "./Ctabutton";
 
 function CtaSection() {
   return (
-    <div className="w-full grid grid-cols-7 md:grid-cols-8 xl:grid-cols-12 gap-x-2 pt-8 pb-8 bg-[url(/svg/ctanoise.svg)] bg-cover bg-top">
-      <h1 className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7">
-        Start Managing Work Without Friction
-      </h1>
-      <p className="xl:col-start-4 xl:col-end-10 md:col-start-3 md:col-end-7 col-start-2 col-end-7 text-center">
+    <div className=" bg-[url(/svg/ctanoise.svg)] bg-cover bg-top h-100 flex flex-col items-center justify-center gap-2 mt-32">
+      <h1 className=" text-center">Start Managing Work Seamlessly</h1>
+      <p className=" text-center text-zinc-600">
         Plan tasks, move work through clear workflows, and stay focused. All in
         one simple, developer-friendly tool.
       </p>
-      <div className="xl:col-start-6 xl:col-end-8 md:col-start-4 md:col-end-6 col-start-4 col-end-5 flex justify-center px-(--pad-1) py-(--pad-5)">
+      <div className="flex justify-center px-(--pad-1) py-(--pad-5) h-max">
         <Ctabutton text={"Start now"} />
       </div>
     </div>
