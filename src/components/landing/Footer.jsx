76477044f4cd0c4ef43e1dd5cc4184ddc7798f5b
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-20 22:01:53 +0530

    added footer animation

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -1,7 +1,16 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 function Footer() {
+  useGSAP(() => {
+    gsap.from(".footer", {
+      yPercent: 50,
+      opacity: 0,
+      duration: 0.8,
+    });
+  }, []);
   return (
     <div className="w-full grid grid-cols-7 md:grid-cols-8 xl:grid-cols-12 gap-2 pt-8">
-      <div className="xl:col-start-3 xl:col-end-11  md:col-start-2 md:col-end-8 col-start-2 col-end-7  xl:text-[180px] md:text-[120px] text-[80px] text-center bg-linear-to-b from-black to-white text-transparent bg-clip-text">
+      <div className=" overflow-hidden footer xl:col-start-3 xl:col-end-11  md:col-start-2 md:col-end-8 col-start-2 col-end-7  xl:text-[180px] md:text-[120px] text-[80px] text-center bg-linear-to-b from-black to-white text-transparent bg-clip-text">
         DEVFLOW
       </div>
     </div>
