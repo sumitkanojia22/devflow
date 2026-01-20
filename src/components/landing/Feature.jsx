b2a3349cf83d78621c0da958ec2071f4c4d5323b
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-20 21:54:35 +0530

    added animation in howitwork section

--- src/components/landing/Feature.jsx
+++ src/components/landing/Feature.jsx
@@ -13,7 +13,7 @@ function Feature() {
       />
       <div className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7 p-4">
         <div className="xl:grid xl:grid-cols-4 xl:grid-rows-8 md:grid md:grid-cols-4 md:grid-rows-8 flex flex-col xl:gap-4 md:gap-4 gap-4 xl:h-150 md:h-150 py-8">
-          <div className="col-span-2 row-span-5 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
+          <div className=" col-span-2 row-span-5 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
             <BentoCards
               imgsrc={"/images/f1.png"}
               title={"Visual Workflow at a Glance"}
@@ -22,21 +22,21 @@ function Feature() {
               }
             />
           </div>
-          <div className="col-span-2 row-span-3 col-start-1 row-start-6 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-200 p-4 hover:scale-[1.02] transition-all ease-in">
-            <BentoCards
-              imgsrc={"/images/f2.png"}
-              title={"Workflows That Make Sense"}
-              para={"Keep work flowing correctly."}
-            />
-          </div>
-          <div className="col-span-2 row-span-3 col-start-3 row-start-1 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
+          <div className=" col-span-2 row-span-3 col-start-3 row-start-1 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
             <BentoCards
               imgsrc={"/images/f3.png"}
               title={"Manage Tasks With Ease"}
               para={"Create, edit, and organize issues directly on the board."}
             />
           </div>
-          <div className="col-span-2 row-span-5 col-start-3 row-start-4 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
+          <div className=" col-span-2 row-span-3 col-start-1 row-start-6 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-200 p-4 hover:scale-[1.02] transition-all ease-in">
+            <BentoCards
+              imgsrc={"/images/f2.png"}
+              title={"Workflows That Make Sense"}
+              para={"Keep work flowing correctly."}
+            />
+          </div>
+          <div className=" col-span-2 row-span-5 col-start-3 row-start-4 overflow-hidden rounded-md shadow-(--shadow-md) border-[0.3px] border-zinc-300 p-4 hover:scale-[1.02] transition-all ease-in">
             <BentoCards
               imgsrc={"/images/f4.png"}
               title={" Find What Matters Instantly"}
--- src/components/landing/HowItWorks.jsx
+++ src/components/landing/HowItWorks.jsx
@@ -1,6 +1,24 @@
+import gsap from "gsap";
 import SectionTitle from "./SectionTitle";
+import { useGSAP } from "@gsap/react";
 
 function HowItWorks() {
+  useGSAP(() => {
+    gsap.from(".card", {
+      scaleY: 0,
+      duration: 0.6,
+      stagger: 0.2,
+      ease: "back.out",
+    });
+
+    gsap.from(".workcard", {
+      yPercent: 5,
+      opacity: 0,
+      duration: 0.8,
+      stagger: 0.2,
+      delay: 0.6,
+    });
+  }, []);
   return (
     <div className="w-full grid grid-cols-7 md:grid-cols-8 xl:grid-cols-12 gap-x-2 gap-y-4 pt-8 pb-16">
       <SectionTitle
@@ -9,17 +27,17 @@ function HowItWorks() {
       />
       {/* Image */}
       {/* <div className=" "> */}
-      <div className=" xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7 rounded-md bg-[url(/svg/feature.svg)] w-full h-64 bg-cover border-[0.2px] border-zinc-200">
-        <div className="flex flex-col items-center justify-end h-full">
-          <div className="bg-bg xl:w-[80%] md:w-[70%] w-[65%] h-16.5 rounded-t-md border-[0.2px] border-zinc-200 "></div>
-          <div className="bg-bg xl:w-[90%] md:w-[80%] w-[75%] h-22.5 rounded-t-md border-[0.2px] border-zinc-200 "></div>
+      <div className=" xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7 rounded-md bg-[url(/svg/feature.svg)] w-full h-64 bg-cover border-[0.2px] border-zinc-200 mb-8">
+        <div className="flex flex-col items-center justify-end h-full relative">
+          <div className="bg-bg xl:w-[80%] md:w-[70%] w-[65%] h-[65%] rounded-t-md border-[0.2px] border-zinc-200 card absolute origin-bottom shadow-2xl shadow-[#7dd421]"></div>
+          <div className="bg-bg xl:w-[90%] md:w-[80%] w-[75%] h-22.5 rounded-t-md border-[0.2px] border-zinc-200 card origin-bottom shadow-2xl shadow-[#7dd421]"></div>
         </div>
       </div>
 
       {/* HowitWorks */}
       <div className="xl:col-start-3 xl:col-end-11 md:col-start-2 md:col-end-8 col-start-2 col-end-7">
         <div className="flex xl:flex-row md:flex-row flex-col justify justify-between gap-2 h-120">
-          <div className="xl:flex-col md:flex-col flex justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
+          <div className="workcard xl:flex-col md:flex-col flex justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
             <div>
               <h4>Create Tasks</h4>
               <p className="text-zinc-500">
@@ -29,7 +47,7 @@ function HowItWorks() {
             </div>
             <h4 className="xl:block md:block hidden">01</h4>
           </div>
-          <div className="flex flex-col justify-between xl:border-l-[0.2px] xl:border-r-[0.2px] md:border-l-[0.2px] md:border-r-[0.2px] border-l-[0.2px] border-zinc-200 p-4">
+          <div className="workcard flex flex-col justify-between xl:border-l-[0.2px] xl:border-r-[0.2px] md:border-l-[0.2px] md:border-r-[0.2px] border-l-[0.2px] border-zinc-200 p-4">
             <div>
               <h4>Move Work</h4>
               <p className="text-zinc-500">
@@ -39,7 +57,7 @@ function HowItWorks() {
             </div>
             <h4 className="xl:block md:block hidden">02</h4>
           </div>
-          <div className="flex flex-col justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
+          <div className="workcard flex flex-col justify-between p-4 border-l-[0.2px] xl:border-l-0 md:border-l-0 border-zinc-200">
             <div>
               <h4>Stay Focused</h4>
               <p className="text-zinc-500">
