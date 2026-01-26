ce8afde79c8f8190194cc6e8162f34a23f2f3548
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-26 21:37:19 +0530

    added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignUp";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
