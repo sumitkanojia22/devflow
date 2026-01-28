5f32b9956231a2b90585593457605441f74c9276
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-01-28 22:34:47 +0530

    added only dashboard page and add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import DashBoard from "./pages/DashBoard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashBoard />} />
     </Routes>
   );
 }
